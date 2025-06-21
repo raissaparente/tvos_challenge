99c246681d637a87e205b7f56d26c34ec28f9fbb
Raissa Parente <raissabparente@gmail.com>
2025-06-21 14:52:37 -0300

    fix: new gameaction struct

--- stop_tv/Sources/Connectivity/GameAction.swift
+++ stop_tv/Sources/Connectivity/GameAction.swift
@@ -7,23 +7,30 @@
 
 import Foundation
 
-struct GameAction: Codable {
-    enum Action: Int, Codable {
-        case sendAnswer
-        case voteAnswer
-        case changeStatus
-    }
-    
-    let action: Action
-    var playerName: String? = nil
-    
-    var status: ConnectionStatus? = nil
-    var category: String? = nil
-    var answer: String? = nil
-    var isAnswerValid: Bool? = nil
-    var currentIndex: Int?
+import Foundation
+
+enum GameActionType: String, Codable {
+    case sendAnswer
+    case voteAnswer
+    case changeStatus
+}
+
+struct GameAction<Payload: Codable>: Codable {
+    let type: GameActionType
+    let payload: Payload
 
     func data() -> Data? {
         try? JSONEncoder().encode(self)
     }
+}
+
+//payloads
+struct SendAnswerPayload: Codable {
+    let playerName: String
+    let answer: String
+}
+
+
+struct ChangeStatusPayload: Codable {
+    let status: ConnectionStatus
 }
